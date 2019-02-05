f6c169a4307352bb4abd1641b1bcf00057f30032
Nicholas Smith <smithn10@rpi.edu>
2019-02-05 15:05:55 -0500

    Button collision script

---
+++ Pet Rock/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour {
+    public GameObject rock;
+    public GameObject player;
+    public GameObject cam;
+    public GameObject connectionObject;
+
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject == rock) {
+            Destroy(connectionObject);
+        }
+    }
+}
