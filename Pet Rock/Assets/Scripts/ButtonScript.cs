65ca67f10602d0fb54b48fff99882be75fe85332
Nicholas Smith <smithn10@rpi.edu>
2019-02-10 13:24:12 -0500

    Adjustments to level0 and fixing button script

--- Pet Rock/Assets/Scripts/ButtonScript.cs
+++ Pet Rock/Assets/Scripts/ButtonScript.cs
@@ -7,22 +7,31 @@ public class ButtonScript : MonoBehaviour {
     public GameObject player;
     public GameObject cam;
     public GameObject connectionObject;
-    private float speed = 0f;
+    public GameObject connector;
+    public float speed = 0f;
+    private bool onButton = false;
+    private bool reachedDestination = false;
 
     void Update() {
-        connectionObject.transform.Translate(Vector3.up * speed * Time.deltaTime);
+        if (onButton) {
+            //reachedDestination = connectionObject.GetComponent<Collider>().IsTouching
+            while (!reachedDestination) {
+                connectionObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            }
+            
+        }
     }
 
     void OnTriggerEnter(Collider col) {
         if (col.gameObject == rock) {
-            speed = 2f;
+            onButton = true;
             rock.SendMessage("DisableFollow");
         }
     }
 
     void OnTriggerExit(Collider col) {
         if (col.gameObject == rock) {
-            speed = 0f;
+            onButton = false;
             rock.SendMessage("EnableFollow");
         }
     }
