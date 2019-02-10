482e30dd773ff0094ac61c0f133342604fb2af9b
Nicholas Smith <smithn10@rpi.edu>
2019-02-10 12:49:00 -0500

    Created level0 and added script for player to interact with levers

---
+++ Pet Rock/Assets/Scripts/LeverScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverScript : MonoBehaviour {
+    public GameObject player;
+    public GameObject cam;
+    public GameObject[] children;
+    private float speed = 0f;
+    private bool inRange = false;
+    private bool leverActive = false;
+
+    void Update() {
+        if (inRange) {
+            if ((Input.GetKeyDown(KeyCode.E)) && (!leverActive)) { // interact button was pressed when lever was not active
+                for(int i=0; i<children.Length; i++) {
+                    children[i].GetComponent<Collider>().enabled = true;
+                    children[i].GetComponent<MeshRenderer>().enabled = true;
+                }
+                leverActive = true;
+            } else if ((Input.GetKeyDown(KeyCode.E)) && (leverActive)) { // interact button was pressed when lever was active
+                for(int i=0; i<children.Length; i++) {
+                    children[i].GetComponent<Collider>().enabled = false;
+                    children[i].GetComponent<MeshRenderer>().enabled = false;
+                }
+                leverActive = false;
+            }
+        }
+    }
+
+
+    void OnTriggerEnter(Collider col) {
+        if (col.name == "Player") { // player collided with object
+            inRange = true; // update in range when entering lever trigger range
+        }
+        Debug.Log("COLLISION");
+    }
+
+    void OnTriggerExit(Collider col) {
+        inRange = false; // update in range when leaving lever trigger range
+    }
+}
