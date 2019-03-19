21dbfaa2788b8af55e426f47af30a200aa38db9d
Nicholas Smith <smithn10@rpi.edu>
2019-03-19 16:53:27 -0400

    Added rock tentacle lever, added more functionality to Level3

--- Pet Rock/Assets/Scripts/BoyAnimationScript.cs
+++ Pet Rock/Assets/Scripts/BoyAnimationScript.cs
@@ -6,9 +6,7 @@ public class BoyAnimationScript : MonoBehaviour {
     public Animator playerAnimator;
 
     void Climbing(bool b) { playerAnimator.SetBool("isClimbing", b); }
-    void Running(bool b) { playerAnimator.SetBool("isRunning", b); }
     void Holding(bool b) { playerAnimator.SetBool("isHolding", b); }
-    void Walking(bool b) { playerAnimator.SetBool("isWalking", b); }
     void Falling(bool b) { playerAnimator.SetBool("isFalling", b); }
     void Landing(bool b) { playerAnimator.SetBool("isLanding", b); }
     void Jumping(bool b) { playerAnimator.SetBool("isJumping", b); }
---
+++ Pet Rock/Assets/Scripts/RockTentacleSwitch.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockTentacleSwitch : MonoBehaviour {
+    public GameObject cam;
+    public GameObject target;
+    public GameObject activeState;
+    public int ticks;
+    public int i = 0;
+    public float speed = 0f;
+    public bool leverActive;
+    private bool inRange = false;
+
+    void Update() {
+        if (inRange && Input.GetKeyDown(KeyCode.E)) {
+            activeState.SetActive(!activeState.activeSelf);
+            activeState.SendMessage("UpdateI", i);
+            gameObject.SetActive(!gameObject.activeSelf);
+        }
+
+        if ((leverActive) && (i < ticks)) {
+            target.transform.Translate(Vector3.right * speed * Time.deltaTime);
+            i++;
+        } else if ((!leverActive) && (i > 0)) {
+            target.transform.Translate(Vector3.left * speed * Time.deltaTime);
+            i--;
+        }
+    }
+
+
+    void OnTriggerEnter(Collider col) {
+        if (col.name == "Rock") {
+            inRange = true; // update in range when entering lever tentacle trigger range
+        }
+    }
+
+    void OnTriggerExit(Collider col) {
+        inRange = false; // update in range when leaving lever tentacle trigger range
+    }
+
+    void UpdateI(int i_) { i = i_; }
+}
