a0bcb256de0a7d2e2ba1f438ff2b0ea07fe1f72a
Nicholas Smith <smithn10@rpi.edu>
2019-04-02 03:10:30 -0400

    Finished all levels, added end level transitions/polishing to Level0

---
+++ Pet Rock/Assets/Scripts/EndLevelLever.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndLevelLever : MonoBehaviour {
+    public GameObject cam;
+    public GameObject rock;
+    public GameObject ladderBlock;
+    public GameObject rockPlatform;
+    public GameObject activeState;
+    public GameObject textBox;
+    public Text textInBox;
+    public Wiring lightingPath = null;
+    public float speed = 0f;
+    public float Distance = 6f;
+    public bool rockInPlace;
+    public bool leverActive;
+    private float DistCovered = 0f;
+    private bool inRange = false;
+
+    void Update() {
+        if ((inRange) && (Input.GetKeyDown(KeyCode.E)) && (!leverActive)) {
+            ladderBlock.SetActive(!ladderBlock.activeSelf);
+            activeState.SetActive(!activeState.activeSelf);
+            gameObject.SetActive(!gameObject.activeSelf);
+            leverActive = !leverActive;
+            if (lightingPath != null) {
+                lightingPath.toggle();
+            }
+        }
+
+        if(leverActive) {
+            float thisMoveDist = Mathf.Min(speed * Time.deltaTime, Distance - DistCovered);
+            rockPlatform.transform.Translate(Vector3.up * thisMoveDist);
+            DistCovered += thisMoveDist;
+            textInBox.text = "";
+        }
+    }
+
+
+    void OnTriggerEnter(Collider col) {
+        if ((col.name == "Character") && (rockInPlace)) { // player collided with object
+            inRange = true; // update in range when entering lever trigger range
+            textBox.SetActive(true);
+            textInBox.text = "Press [E] to use lever";
+        }
+    }
+
+    void OnTriggerExit(Collider col) {
+        inRange = false; // update in range when leaving lever trigger range
+        textBox.SetActive(false);
+    }
+}
---
+++ Pet Rock/Assets/Scripts/RockPlatformTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockPlatformTrigger : MonoBehaviour {
+    public GameObject rock;
+    public EndLevelLever leverActive;
+    public EndLevelLever leverInactive;
+
+    void OnTriggerEnter(Collider col) {
+        if (col.gameObject == rock) {
+            leverActive.rockInPlace = true;
+            leverInactive.rockInPlace = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        leverActive.rockInPlace = false;
+        leverInactive.rockInPlace = false;
+    }
+}
