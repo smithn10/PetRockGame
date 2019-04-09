bb8d6e04bd81f8f060d0a1599907f0820e6c9d15
Nicholas Smith <smithn10@rpi.edu>
2019-04-09 15:00:15 -0400

    Added feedback about new ability when rock evolves& fixed text in level0

--- Pet Rock/Assets/Scripts/LevelEndGateOpener.cs
+++ Pet Rock/Assets/Scripts/LevelEndGateOpener.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelEndGateOpener : MonoBehaviour {
     public GameObject player;
     public GameObject rock;
     public GameObject endGate;
+    public TextAsset[] files;
     public int stage = 0;
     public int enemyCount;
     private bool opened = false;
+    private DialogueManager dialogueManager;
+    
+    void Start() {
+        dialogueManager = GetComponent<DialogueManager>(); // get dialogue manager
+    }
 
     // Update is called once per frame
     void Update() {
@@ -24,17 +31,20 @@ public class LevelEndGateOpener : MonoBehaviour {
                     rock.transform.GetChild(0).gameObject.SetActive(false);
                     rock.transform.GetChild(1).gameObject.SetActive(true);
                     rock.GetComponent<BoyAnimationScript>().Evolve();
+                    GiveNewTip(files[0]);
                     break;
                 case 1:
                     rock.GetComponent<CharControl>().glideEnabled = true;
                     rock.transform.GetChild(1).gameObject.SetActive(false);
                     rock.transform.GetChild(2).gameObject.SetActive(true);
                     rock.GetComponent<BoyAnimationScript>().Evolve();
+                    GiveNewTip(files[1]);
                     break;
                 case 2:
                     rock.transform.GetChild(2).gameObject.SetActive(false);
                     rock.transform.GetChild(3).gameObject.SetActive(true);
                     rock.GetComponent<BoyAnimationScript>().Evolve();
+                    GiveNewTip(files[2]);
                     break;
                 case 3:
                     rock.transform.GetChild(3).gameObject.SetActive(false);
@@ -46,4 +56,10 @@ public class LevelEndGateOpener : MonoBehaviour {
     }
 
     void DecreaseCount() { enemyCount--; }
+
+    void GiveNewTip(TextAsset file) {
+        dialogueManager.Reload(file); // reload function described in DialogueManager script
+        dialogueManager.currLineIndex = 0;
+        dialogueManager.EnableTextBox();
+    }
 }
