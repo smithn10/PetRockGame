fabc7bb1fe5b1d492e82ecb31fa711a79e3403ee
Nicholas Smith <smithn10@rpi.edu>
2019-04-12 18:20:25 -0400

    Added main menu scene

---
+++ Pet Rock/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour {
+
+    public void PlayGame() {
+        SceneManager.LoadScene("Level0");
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
