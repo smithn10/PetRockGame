683a7a05da452a153d769e7dd7189a6547480f5b
Nicholas Smith <smithn10@rpi.edu>
2019-04-18 13:15:37 -0400

    Fixed bite animation in cutscene

--- Pet Rock/Assets/Scripts/FinalRockBite.cs
+++ Pet Rock/Assets/Scripts/FinalRockBite.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class FinalRockBite : MonoBehaviour {
     public FinalScript finalScript;
+    public bool b;
 
     void OnEnable() {
-        finalScript.RockBite(true);
-        finalScript.RockBite(false);
+        finalScript.RockBite(b);
     }
 }
